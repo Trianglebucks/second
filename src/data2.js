8da3859e2a2c858e652f28e65fbd794edc98e588
William <blazerx1234@gmail.com>
2021-10-10 19:05:07 +0800

    g

---
+++ src/data2.js
@@ -0,0 +1,139 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "I",
+    price: 15.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855480/sixteen_sddcef.jpg",
+    desc: `Taray naman dito baby, eto ung recent nating gala `
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "I",
+    price: 13.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855479/December_xgdy2m.jpg",
+    desc: `Twinnings yern, usto ka na uli ng new shoes wieieieie `
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "I",
+    price: 6.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855476/six_rmuh2i.jpg",
+    desc: `Awww, kelan kaya uli us mini gala, kamiss naman :<<.`
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "I",
+    price: 20.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855474/five_pyaosl.jpg",
+    desc: `Kauna unahan kong gana na pumorma para sa isang tao HAHAHAHA kek `
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "I",
+    price: 22.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855475/seventeen_c0njzy.jpg",
+    desc: `Yieieieieie unang samgyup ko kek, pero sayang di nasulit HAHA, pero busog uwu `
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "I",
+    price: 18.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855474/seven_p6hxb5.jpg",
+    desc: `Somewhere in west ave, there's two people trapped in a waiting shed due to the rain~`
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "LOVE",
+    price: 8.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855474/fifteen_kzhzmv.jpg",
+    desc: `gala gala as usual, kelan kaya uli hais, usto ko narin new clothesss`
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "LOVE",
+    price: 12.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855474/one_sllzjv.jpg",
+    desc: `dito ka raw na unang na starstruck sakin hehe  `
+  },
+  {
+    id: 10,
+    title: "quarantine buddy",
+    category: "LOVE",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855471/twelve_y8yvby.jpg",
+    desc: `Panesss, ang cool natin dto HAHAHAHA, ginawang skin sa keyboard xdd.`
+  },
+  {
+    id: 11,
+    title: "quarantine buddy",
+    category: "LOVE",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855473/four_bharex.jpg",
+    desc: `Eto ung nagpabuhat you nung speakerrr, cute ko rito wink`
+  },
+  {
+    id: 12,
+    title: "quarantine buddy",
+    category: "LOVE",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855473/three_c3l0c4.jpg",
+    desc: `Another one of those mini galaa, sa kanluran to ehhh.`
+  },
+  {
+    id: 13,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855473/nine_ugy91v.jpg",
+    desc: `Sayanggg, di na nakakatambay dyan sa house nyooo, lam mo na un hehe`
+  },
+  {
+    id: 14,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855472/eleven_nw90ai.jpg",
+    desc: `eto ung di natuloy ung alis mo ata un hahahaha, cowla natin dto ghurl`
+  },
+  {
+    id: 15,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855472/fourteen_znxwmk.jpg",
+    desc: `ang bagong gupit na lalaki vs ang pixie cut na babae`
+  },
+  {
+    id: 16,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855471/eight_tjauhk.jpg",
+    desc: `Eto ata ung pinakauna kong pag simba or prolly pangalawa dun sa church nyoo`
+  },
+  {
+    id: 17,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855471/two_cgfp3q.jpg",
+    desc: `Wawii, hesitant talaga ako sumama nung una dto HAHAHA, pero cowlaaaa, nameet ko mga circle of friends mo wieee`
+  },
+  {
+    id: 18,
+    title: "quarantine buddy",
+    category: "YOU",
+    price: 16.99,
+    img: "https://res.cloudinary.com/dkhziojjb/image/upload/v1633855471/thirteen_tpd1mk.jpg",
+    desc: `Pogi mo naman dito pare, krazyy, mamahalin kita pag lalaki ka.`
+  }
+];
+export default menu;
